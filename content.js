ff555689f1135af7dfc361635ce9035175718883
caleanunoah <noahcaleanu152@gmail.com>
2021-05-24 17:53:28 -0700

    banned websites detected

--- content.js
+++ content.js
@@ -3,6 +3,14 @@ console.log("Running Content.js")
 //console.log("Number 1", currentLocation); 
 
 
+var banned_names = [
+  "benjerrys",
+  "benjerry",
+  "ben-jerry",
+  "benandjerrys",
+  "benandjerry",
+  "ben-jerrys",
+]
 
 
 
@@ -12,13 +20,14 @@ window.onload = (event) => {
   //console.log(currentLocation);
   // split url here
   let company_name = currentLocation.split(".")[1]
-  console.log(company_name)
+  console.log(company_name);
 
-  // load all database URLs
-  //[url_page] = load("/path/file.js")
+  if (banned_names.includes(company_name)){
+    alert("This company supports Israel");
+  }
 
   //for url in url_page:
   //  if company_name == url:
   //    alert("BAD")
 };
-  
+
